1b182b93c81c37911cde0443638876616fab95f2
robmortell <36989223+robmortell@users.noreply.github.com>
2024-10-27 17:23:18 +0000

    First push - Added Module-1 folder, and Mod-1-Ex-1 file.

---
+++ Module-1/Mod-1-Ex-1.py
@@ -0,0 +1,19 @@
+# Write your code below this line 👇
+
+# This is just going to be basic printing and getting used to syntax
+
+from time import sleep
+
+print("Hello World")
+sleep(1)
+print("Hello World again, after 1 second sleep")
+sleep(1)
+print("Hello","World")
+sleep(1)
+print("You can use single or double quotes, it doesn't matter")
+sleep(1)
+print('But if you want to use a single quote IN your statement, you need to enclose the whole thing in double quotes')
+sleep(1)
+print("I'd much rather you 'not'.")
+sleep(1)
+print('I said "DO NOT TOUCH THAT".')
