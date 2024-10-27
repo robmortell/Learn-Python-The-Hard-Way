87a30f6af0e5c1c43bc969de21cd85d683d0c4b8
robmortell <36989223+robmortell@users.noreply.github.com>
2024-10-27 17:51:51 +0000

    First push - Added Module-1 folder, and Mod-1-Ex-1 file.

---
+++ Module-1/Mod-1-Ex-2-Comments & Pound Characters.py
@@ -0,0 +1,17 @@
+# Write your code below this line 👇
+
+# Basic commenting
+
+from time import sleep
+
+print("I can have code like this")
+sleep(1)
+print("I can also have code like this") # everything after the hash is ignored
+sleep(1)
+print("This will run")
+sleep(1)
+# print("But this line will not run, until I delete the hash at the start of the line")
+sleep(1)
+print("Will this # character be recognised as a character, or a comment...?") # it is recognised as a character because it is part of a string
+sleep(1)
+print("It is recognised as a character and printed, because it is part of a string...!")
